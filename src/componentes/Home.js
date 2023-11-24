9f38b2cd29eccaaee43c3a5a8e36a1b14d4044b0
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-24 11:01:28 +0100

    Update-Portfolio

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className='w-full h-screen  bg-[#0a192f]'>
-      
+    <div name="home" className="w-full h-screen  bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hola, mi nombre es</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Juan Carlos Sánchez
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          Soy un Web Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Hola soy un desarrollador web, que tiene diferentes experiencias y ha
+          estudiado Ingeniería de Sonido e Imagen en la Universidad Politécnica
+          de Madrid y una FP superior de Desarrollador de Aplicaciones Web.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
