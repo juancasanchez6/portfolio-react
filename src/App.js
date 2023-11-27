b3697474ba23fde5c02515380aea4cbcd2833338
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-27 11:22:29 +0100

    Update-Portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import About from "./componentes/About";
 import Home from "./componentes/Home";
 import Navbar from "./componentes/Navbar";
+import Skills from "./componentes/Skills";
 
 function App() {
   return (
     <div>
      <Navbar />
      <Home />
+     <About />
+     <Skills />
     </div>
   );
 }
