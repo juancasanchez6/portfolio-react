a505c7c153581f3a12413bea03d79696600eb5ab
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-28 10:27:31 +0100

    Update-Portafolio

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -86,11 +86,56 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+        <Link
+            onClick={handleClick}
+            to="home"
+            smooth={true}
+            duration={500}
+          >
+            Inicio
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+        <Link
+            onClick={handleClick}
+            to="about"
+            smooth={true}
+            duration={500}
+          >
+            Sobre mi
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+        <Link
+            onClick={handleClick}
+            to="skills"
+            smooth={true}
+            duration={500}
+          >
+            Experiencia
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+        <Link
+            onClick={handleClick}
+            to="work"
+            smooth={true}
+            duration={500}
+          >
+            Proyectos
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+        <Link
+            onClick={handleClick}
+            to="contact"
+            smooth={true}
+            duration={500}
+          >
+            Contacto
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
--- src/componentes/Skills.js
+++ src/componentes/Skills.js
@@ -10,8 +10,8 @@ const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f]  text-gray-300'>
         {/* Contenedor */}
-      <div className='max-w-[1000px] mx-auto flex flex-col w-full h-full'>
-        <div className='mx-2'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experiencia</p>
             <p className='py-4'>Estas son las destrezas con las cuales he trabajado </p>
         </div>
