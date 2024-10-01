c3109928c4c0d559ba26073135e46a527f5655dc
juancasanchez6 <jcsanchezr04@educarex.es>
2024-10-01 12:18:37 +0200

    arreglado link email

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -160,7 +160,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-            <Link to="contact" smooth={true} duration={500}>
+            <Link className="flex justify-between items-center w-full text-gray-300" to="contact" smooth={true} duration={500}>
               <a
                 className="flex justify-between items-center w-full text-gray-300"
                 href="/"
